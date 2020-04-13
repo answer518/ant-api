7bb9e6e4a76383af4b97e92137b6d4c739d7616c
果糖酱 <iamnabob@126.com>
2020-04-14 00:07:05 +0800

    首页接口开发

--- src/model/User.js
+++ src/model/User.js
@@ -3,11 +3,107 @@ import mongoose from '../config/DBHelpler'
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  username: { type: String },
-  nickname: { type: String },
+  username: { type: String, index: { unique: true }, sparse: true },
   password: { type: String },
-  created: { type: Date }
+  name: { type: String },
+  created: { type: Date },
+  updated: { type: Date },
+  favs: { type: Number, default: 100 },
+  gender: { type: String, default: '' },
+  roles: { type: Array, default: ['user'] },
+  pic: { type: String, default: '/img/header.jpg' },
+  mobile: { type: String, match: /^1[3-9](\d{9})$/, default: '' },
+  status: { type: String, default: '0' },
+  regmark: { type: String, default: '' },
+  location: { type: String, default: '' },
+  isVip: { type: String, default: '0' },
+  count: { type: Number, default: 0 }
 })
+
+UserSchema.pre('save', function (next) {
+  this.created = new Date()
+  next()
+})
+
+UserSchema.pre('update', function (next) {
+  this.updated = new Date()
+  next()
+})
+
+UserSchema.post('save', function (error, doc, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    next(new Error('Error: Monngoose has a duplicate key.'))
+  } else {
+    next(error)
+  }
+})
+
+UserSchema.statics = {
+  findByID: function (id) {
+    return this.findOne(
+      { _id: id },
+      {
+        password: 0,
+        username: 0,
+        mobile: 0
+      }
+    )
+  },
+  getList: function (options, sort, page, limit) {
+    // 1. datepicker -> item: string, search -> array  startitme,endtime
+    // 2. radio -> key-value $in
+    // 3. select -> key-array $in
+    let query = {}
+    if (typeof options.search !== 'undefined') {
+      if (typeof options.search === 'string' && options.search.trim() !== '') {
+        if (['name', 'username'].includes(options.item)) {
+          // 模糊匹配
+          query[options.item] = { $regex: new RegExp(options.search) }
+          // =》 { name: { $regex: /admin/ } } => mysql like %admin%
+        } else {
+          // radio
+          query[options.item] = options.search
+        }
+      }
+      if (options.item === 'roles') {
+        query = { roles: { $in: options.search } }
+      }
+      if (options.item === 'created') {
+        const start = options.search[0]
+        const end = options.search[1]
+        query = { created: { $gte: new Date(start), $lt: new Date(end) } }
+      }
+    }
+    return this.find(query, { password: 0, mobile: 0 })
+      .sort({ [sort]: -1 })
+      .skip(page * limit)
+      .limit(limit)
+  },
+  countList: function (options) {
+    let query = {}
+    if (typeof options.search !== 'undefined') {
+      if (typeof options.search === 'string' && options.search.trim() !== '') {
+        if (['name', 'username'].includes(options.item)) {
+          // 模糊匹配
+          query[options.item] = { $regex: new RegExp(options.search) }
+          // =》 { name: { $regex: /admin/ } } => mysql like %admin%
+        } else {
+          // radio
+          query[options.item] = options.search
+        }
+      }
+      if (options.item === 'roles') {
+        query = { roles: { $in: options.search } }
+      }
+      if (options.item === 'created') {
+        const start = options.search[0]
+        const end = options.search[1]
+        query = { created: { $gte: new Date(start), $lt: new Date(end) } }
+      }
+    }
+    return this.find(query).countDocuments()
+  }
+}
 
 const UserModel = mongoose.model('users', UserSchema)
 
